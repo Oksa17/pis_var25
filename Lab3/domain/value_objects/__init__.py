3f540cd2cd69d4612d9543f18060963b70dc666b
Oksa17 <144802817+Oksa17@users.noreply.github.com>
2026-04-24 00:07:01 +0300

    Create __init__.py

---
+++ Lab3/domain/value_objects/__init__.py
@@ -0,0 +1,5 @@
+from domain.value_objects.task_status import TaskStatus
+from domain.value_objects.priority import Priority
+from domain.value_objects.assignment_info import AssignmentInfo
+
+__all__ = ['TaskStatus', 'Priority', 'AssignmentInfo']
